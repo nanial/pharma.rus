085c5e82fa58a1435f46d96d3112e4b89dfad611
nanial <nanial@mail.ru>
2018-07-14 14:44:36 +0300

    00000000000

--- views/site/index.php
+++ views/site/index.php
@@ -1,107 +1,59 @@
 <?php include ROOT . '/views/layouts/header.php'; ?>
 
+
+<style>
+    .item2{
+/*стиль блока навигации*/
+     margin-left:4px;
+     margin-top:5px;
+     overflow:hidden;
+     
+}
+    .cycle-slideshow img {
+        height: 100% !important; 
+        width: 100% !important;
+    }
+    .panel-group category-products{
+        height: 150px !important; 
+        width: 50px !important;
+    }
+    
+</style>
 <section>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-3">
-                <div class="left-sidebar">
-                    <h2>Каталог</h2>
-                    <div class="panel-group category-products">
-                        <?php foreach ($categories as $categoryItem): ?>
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title">
-                                        <a href="/category/<?php echo $categoryItem['ID']; ?>">
-                                            <?php echo $categoryItem['name']; ?>
-                                        </a>
-                                    </h4>
-                                </div>
-                            </div>
-                        <?php endforeach; ?>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="col-sm-9 padding-right">
-                <div class="features_items">
-                    <h2 class="title text-center">Последние товары</h2>
+    <div class="page-wrapper">
+        <div class="container">
+            <div class="row">
+                
+        <div>
 
-                    <?php foreach ($latestProducts as $product): ?>
-                        <div class="col-sm-4">
-                            <div class="product-image-wrapper">
-                                <div class="single-products">
-                                    <div class="productinfo text-center">
-                                        <img src="<?php echo $product['image'] ?>" alt="" />
+            <div class="cycle-slideshow"
+                data-cycle-fx=carousel
+                data-cycle-timeout=22000
+                data-cycle-carousel-visible=1
+                data-cycle-carousel-fluid=true
+                data-cycle-slides="div.item2"
+                data-cycle-prev="#prev"
+                data-cycle-next="#next"
+            >              
                                         
-                                        <h2> <?php echo $product['price']; ?> бел.руб</h2>
-                                        <p>
-                                            <a href="/product/<?php echo $product['ID']; ?>">
-                                                <?php echo $product['nameOfMedical']; ?>
-                                            </a>
-                                        </p>
-                                        <a href="#" class="btn btn-default add-to-cart" data-id="<?php echo $product['ID']; ?>"><i class="fa fa-shopping-cart"></i>Купить</a>
+                                    <?php foreach ($sliderImages as $sliderImages): ?>
+                                    <div class="item2">
+                                    
+                                         <?php echo $sliderImages['ID']; ?>
+                                                    <img src="<?php echo $sliderImages['imageSlide']; ?>" alt="" />
+                                                    </a>
+                                                
+                                    
                                     </div>
-                                    <?php if ($product['isNew']): ?>
-                                        <img src="/template/images/home/new.png" class="new" alt="" />
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
-
-                <?php echo $pagination->get(); ?>
-                </div>               
-        
-                <div class="recommended_items"><!--Отечественные_items-->
-                    <h2 class="title text-center">Отечественные препараты </h2>
-                    
-                    <div class="cycle-slideshow" 
-                         data-cycle-fx=carousel
-                         data-cycle-timeout=5000
-                         data-cycle-carousel-visible=3
-                         data-cycle-carousel-fluid=true
-                         data-cycle-slides="div.item"
-                         data-cycle-prev="#prev"
-                         data-cycle-next="#next"
-                         >              
-                                  
-                             <?php foreach ($sliderProducts as $sliderItem): ?>
-                            <div class="item">
-                                <div class="product-image-wrapper">
-                                    <div class="single-products">
-                                        <div class="productinfo text-center">
-                                       
-                                            
-                                            <a href="/product/<?php echo $sliderItem['ID']; ?>">
-                                            <img src="<?php echo $sliderItem['image']; ?>" alt="" />
-                                            </a>
-                                            <h4> <?php  echo $sliderItem["nameOfMedical"];?></h4>
-                                            <h2><?php echo $sliderItem['price']; ?> бел.руб </h2>
-                                            <br/><br/>
-                                            <a href="#" class="btn btn-default add-to-cart" data-id="<?php echo $sliderItem['ID']; ?>"><i class="fa fa-shopping-cart"></i>Купить</a>
-                                        </div>
-                                        <?php if ($sliderItem['isNew']): ?>
-                                            <img src="/template/images/home/new.png" class="new" alt="" />
-                                        <?php endif; ?>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php endforeach; ?>
-                    </div>
-
-                    <a class="left recommended-item-control" id="prev" href="#recommended-item-carousel" data-slide="prev">
-                        <i class="fa fa-angle-left"></i>
-                    </a>
-                    <a class="right recommended-item-control" id="next"  href="#recommended-item-carousel" data-slide="next">
-                        <i class="fa fa-angle-right"></i>
-                    </a>
-
-                   </div>
-              </div><!--/Отечественные_items-->
-
+                                <?php endforeach; ?>
+                            
+            </div> 
+           
         </div>
-    </div>
-
+    </div> 
+</div>
+</div>      
+</div>
 </section>
-
 <?php include ROOT . '/views/layouts/footer.php'; ?>
+
